f3f1ee07f32dcdf5f15ba1b5c5747c3adc8e32ca
Nicolas F <nicolas.funke@amplifierstrategies.com>
2018-08-06 15:12:51 -0700

    Update

--- _grunt/watch.js
+++ _grunt/watch.js
@@ -1,19 +1,18 @@
 module.exports = {
-	sass: {
-        files: '_src/scss/**/*.scss',
-        tasks: ['sass', 'shell:jekyllBuild']
-    },
-    concat: {
-        files: ['_src/js/**/*.js', '_src/js/main.js'],
-        tasks: ['concat']
-    },
-    uglify: {
-    	files: '_src/js/app.js',
-    	tasks: ['uglify']
-    },
-    jekyll: {
-        files: ['_layouts/*.html', '_includes/*.md', '_includes/*.html', 'css/main.css', '*.html', '*.md', 'assets/js/app.min.js'],
-        tasks: ['shell:jekyllBuild']
-    }
+  sass: {
+    files: '_src/scss/**/*.scss',
+    tasks: ['sass', 'shell:jekyllBuild']
+  },
+  concat: {
+    files: ['_src/js/**/*.js', '_src/js/main.js'],
+    tasks: ['concat']
+  },
+  uglify: {
+    files: '_src/js/app.js',
+    tasks: ['uglify']
+  },
+  jekyll: {
+    files: ['_layouts/*.html', '_includes/*.md', '_includes/*.html', 'css/main.css', '*.html', '*.md', 'assets/js/app.min.js'],
+    tasks: ['shell:jekyllBuild']
+  }
 }
-
