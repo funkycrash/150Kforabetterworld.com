4aee02d2cfccabe831b52b741fff753b449a1fa3
Nicolas F <nicolas.funke@amplifierstrategies.com>
2018-08-15 13:09:11 -0700

    Adding muted video by default

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,33 +1,12 @@
 
 $(document).ready(function () {
-  // Text effect on first slide
-  // Wrap every letter in a span
-  $('.ml12').each(function () {
-    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"))
-  })
 
-  var animateFirstPageText = function () {
-    anime.timeline({loop: false})
-      .add({
-        targets: '.ml12 .letter',
-        translateX: [40, 0],
-        translateZ: 0,
-        opacity: [0, 1],
-        easing: 'easeOutExpo',
-        duration: 1200,
-        delay: function (el, i) {
-          return 500 + 30 * i
-        }
-      })
-  }
 
   // Loading the music
   var sound = new Howl({
     src: ['/assets/music/background_music.mp3'],
     loop: true,
     onload: function () {
-      // Animate the text once all is loaded
-      animateFirstPageText()
       $('#first').show()
       // Slideshow initialization
       Reveal.initialize({
