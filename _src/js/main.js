ca60ed453324bb221f6143f53ea8e9bbdb8fec9b
Nicolas F <nicolas.funke@amplifierstrategies.com>
2018-08-23 18:03:47 -0700

    Add carousel in last slide

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,7 +1,5 @@
 
 $(document).ready(function () {
-
-
   // Loading the music
   var sound = new Howl({
     src: ['/assets/music/background_music.mp3'],
@@ -21,7 +19,7 @@ $(document).ready(function () {
           $(event.fragment).find('img').addClass('animation-target')
         }
       })
-      Reveal.addEventListener( 'fragmenthidden', function( event ) {
+      Reveal.addEventListener('fragmenthidden', function (event) {
         if ($(event.fragment).hasClass('bounce')) {
           $(event.fragment).find('img').removeClass('animation-target')
         }
@@ -42,6 +40,72 @@ $(document).ready(function () {
     }
   })
 
+  // #########################################
+  // ##
+  // ## Carousel
+  // ##
+  // #########################################
+  var carousel = document.querySelector('.carousel')
+  var cells = carousel.querySelectorAll('.carousel__cell')
+  var cellCount // cellCount set from cells-range input value
+  var selectedIndex = 0
+  var cellWidth = carousel.offsetWidth
+  var cellHeight = carousel.offsetHeight
+  var isHorizontal = true
+  var rotateFn = isHorizontal ? 'rotateY' : 'rotateX'
+  var radius, theta
+  // console.log( cellWidth, cellHeight );
+  
+  function rotateCarousel () {
+    var angle = theta * selectedIndex * -1
+    carousel.style.transform = 'translateZ(' + -radius + 'px) ' +
+      rotateFn + '(' + angle + 'deg)'
+  }
+
+  var prevButton = document.querySelector('.previous-button')
+  prevButton.addEventListener('click', function () {
+    selectedIndex--
+    rotateCarousel()
+  })
+  
+  var nextButton = document.querySelector('.next-button')
+  nextButton.addEventListener('click', function () {
+    selectedIndex++
+    rotateCarousel()
+  })
+  
+  var cellsRange = document.querySelector('.cells-range')
+  cellsRange.addEventListener('change', changeCarousel)
+  cellsRange.addEventListener('input', changeCarousel)
+  
+  function changeCarousel () {
+    cellCount = cellsRange.value
+    theta = 360 / cellCount
+    var cellSize = isHorizontal ? cellWidth : cellHeight
+    radius = Math.round((cellSize / 2) / Math.tan(Math.PI / cellCount))
+    for (var i = 0; i < cells.length; i++) {
+      var cell = cells[i]
+      if (i < cellCount) {
+        // visible cell
+        cell.style.opacity = 1
+        var cellAngle = theta * i
+        cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + 180 + 'px)'
+      } else {
+        // hidden cell
+        cell.style.opacity = 0
+        cell.style.transform = 'none'
+      }
+    }
+
+    rotateCarousel()
+  }
+  // set initials
+  changeCarousel()
+  setInterval(function () {
+    selectedIndex++
+    rotateCarousel()
+  }, 10000)
+  
   // #########################################
   // ##
   // ## Functional Javascript
